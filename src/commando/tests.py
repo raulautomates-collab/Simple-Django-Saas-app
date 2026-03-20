14ceb0ac550e744ba78ee8ec2120810cbd5fccca
Saul Goodman <raulautomates@gmail.com>
2026-03-20 21:52:41 +0300

    4_ADDED database url

--- src/commando/tests.py
+++ src/commando/tests.py
@@ -1,5 +1,11 @@
+
 from django.test import TestCase
+from django.conf import settings
 
 # Create your tests here.
 #testing the database url teet cse
 
+class NeonDatabaseTestCase(TestCase):
+    def test_db_url(self):
+      DATABASE_URL=settings.DATABASE_URL
+      self.assertIn('neon.tech',DATABASE_URL)
