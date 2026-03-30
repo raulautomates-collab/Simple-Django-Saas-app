ea0cfe9497423d7ce848114791d6699e3a38e3e5
Saul Goodman <raulautomates@gmail.com>
2026-03-30 16:30:13 +0300

    rolled back the views to a previous working version

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -44,9 +44,9 @@ SECRET_KEY = str(os.environ.get('DJANGO_SECRET_KEY'))
 # Default (no need to add this unless customizing)
 AUTH_USER_MODEL = 'auth.User'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  str(os.environ.get('DJANGO_DEBUG'))
+DEBUG =  str(os.environ.get('DJANGO_DEBUG',default=False))
 BASE_URL=os.environ.get('BASE_URL',default=None)
-ALLOWED_HOSTS = ['.railway.app']
+ALLOWED_HOSTS = []
 if DEBUG:
     ALLOWED_HOSTS+=[
         '127.0.0.1',
